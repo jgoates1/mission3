e5f69ed27f5a28eb3abea83ad2de2dd8507f451f
jgoates1 <jgoates1@byu.edu>
2026-01-21 21:36:23 -0700

    mission 4

---
+++ FoodItem.cs
@@ -0,0 +1,25 @@
+namespace mission4;
+
+using System;
+
+public class FoodItem
+{
+    public string foodName { get; set; }
+    public string foodCategory { get; set; }
+    public int foodQuantity { get; set; }
+    public DateOnly foodExpiration { get; set; }
+
+    public FoodItem(string foodName, string foodCategory, int foodQuantity, DateOnly foodExpiration)
+    {
+        this.foodName = foodName;
+        this.foodCategory = foodCategory;
+        this.foodQuantity = foodQuantity;
+        this.foodExpiration = foodExpiration;
+    }
+
+    public override string ToString()
+    {
+        return $"{foodName}, {foodCategory}, {foodQuantity}, Expires: {foodExpiration}";
+    }
+    
+}
